e5248516d7581af672486d304badcf35f72d2eda
shroyash <shroyash35@gmail.com>
2025-08-07 21:53:50 +0545

    microservice all setup

---
+++ ai-diagnostics-service/src/main/java/com/example/ai_diagnostics_service/AiDiagnosticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_diagnostics_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDiagnosticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDiagnosticsServiceApplication.class, args);
+	}
+
+}
