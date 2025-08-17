7f3ba684d37fc6ee85a42228a5078624255a5d2e
shroyash <shroyash35@gmail.com>
2025-08-17 21:21:53 +0545

    dockor configured

--- doctor-service/src/main/java/com/example/doctor_service/DoctorServiceApplication.java
+++ doctor-service/src/main/java/com/example/doctor_service/DoctorServiceApplication.java
@@ -3,11 +3,11 @@ package com.example.doctor_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DoctorServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorServiceApplication.class, args);
 	}
-
 }
