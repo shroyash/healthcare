598d3f3d088fbf1f348e43910ad4f6841734ea0b
shroyash <shroyash35@gmail.com>
2025-08-27 21:06:31 +0545

    api gate-way configured

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE doctor_service_db;
+CREATE DATABASE appointment_service_db;
+CREATE DATABASE auth_service_db;
+CREATE DATABASE batch_service_db;
+CREATE DATABASE billing_service_db;
+CREATE DATABASE patient_service_db;
+CREATE DATABASE search_service_db;
